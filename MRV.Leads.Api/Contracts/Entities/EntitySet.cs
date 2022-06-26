dbd328825dd5f5dd207b39c1db5f0cb38197392d
Guilherme <guilherme.julio@d20labs.tech>
2022-06-26 14:01:34 -0300

    Cors policy

--- MRV.Leads.Api/Contracts/Entities/EntitySet.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MRV.Leads.Api.Contracts.Entities;
-
-public abstract class EntitySet
-{
-    public Guid Id { get; set; }
-}
--- MRV.Leads.Api/Contracts/Entities/ListSet.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MRV.Leads.Api.Contracts.Entities;
-
-public abstract class ListSet
-{
-    public int TotalPages { get; set; } = 1;
-    public int TotalRows { get; set; } = 1;
-    public int Offset { get; set; } = 1;
-    public int? Limit { get; set; }
-}
